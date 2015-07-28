b3fa70fc97789b9ab73da1d7632aebeef6dfba4d
haithamsaadoon <haith_engi@yahoo.com>
2015-07-28 14:43:10 +0300

    Merge origin/master
    Conflicts:
    	resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header" style="padding:35px 50px;">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <button type="button" class="close" data-dismiss="modal"></button>
           <h4><span class="glyphicon glyphicon-lock"></span> تسجيل دخول</h4>
         </div>
         <div class="modal-body" style="padding:40px 50px;">
@@ -48,13 +48,15 @@
             <div class="checkbox">
               <label><input type="checkbox" value="" checked>Remember me</label>
             </div>
-              <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+              <button type="submit" class="btn btn-success btn-block"> تسجيل دخول<span class="glyphicon glyphicon-off"></span></button>
           </form>
         </div>
         <div class="modal-footer">
           <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-          <p>Not a member? <a href="#">Sign Up</a></p>
-          <p>Forgot <a href="#">Password?</a></p>
+          <p> <a href="#">تسجيل جديد</a>
+          لست مستخدم حالي؟
+          </p>
+          <p><a href="#">كلمة المرور</a> نسيت كلمة السر؟</p>
         </div>
       </div>
       
